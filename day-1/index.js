8c33963e75ce3b575f917526834ceef6af64e624
Daniel Apt <danieapt@amazon.com>
2022-12-01 09:01:53 +0000

    Complete day 1 puzzle

---
+++ day-1/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const sampleInput = fs.readFileSync('./sample-input.txt', 'utf8');
+const puzzleInput = fs.readFileSync('./input.txt', 'utf8');
+
+console.log(main(sampleInput));
+console.log(main(puzzleInput));
+
+/**
+ * @param {String} puzzleInput
+ * @return {Number} calories of largest amount
+ */
+function main(puzzleInput) {
+    // split it into items
+    const items = (puzzleInput.split('\n').map(str => Number(str)));
+
+    const carriedCaloriesList = [];
+
+    let currentElf = 0;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === 0) {
+            carriedCaloriesList.push(currentElf);
+            currentElf = 0;
+            continue;
+        }
+
+        currentElf += items[i];
+    }
+
+    carriedCaloriesList.sort((a, b) => b - a);
+
+    return carriedCaloriesList[0] +  carriedCaloriesList[1] + carriedCaloriesList[2];
+}
