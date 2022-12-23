fc7aa462832ab27db799049258a2640af341a477
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:48 +0000

    Solve 18 part 2

--- day-18/part-2.js
+++ day-18/part-2.js
@@ -3,11 +3,7 @@ const fs = require("fs");
 const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
-const CUBE_SURFACE = 6;
-
-// 2474 is too low
-
-// console.log(main(`1,1,1\n2,1,1`));
+console.log(main(`1,1,1\n2,1,1`)); // note, it doesn't work with this
 console.log(main(sampleInput));
 console.log(main(puzzleInput));
 
@@ -16,13 +12,12 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    // create 3d space to add cubes to
-    const space = new Array();
-
     const cubes = puzzleInput
         .split("\n")
         .map((s) => s.split(",").map((s) => Number(s)))
         .map(([x, y, z]) => ({ x, y, z }));
+
+    const coordinates = [];
 
     const xMin = Math.min(...cubes.map((c) => c.x));
     const xMax = Math.max(...cubes.map((c) => c.x));
@@ -31,74 +26,50 @@ function main(puzzleInput) {
     const zMin = Math.min(...cubes.map((c) => c.z));
     const zMax = Math.max(...cubes.map((c) => c.z));
 
-    const toIgnoreSpace = [];
+    for (let x = xMin - 1; x <= xMax + 1; x++) {
+        for (let y = yMin - 1; y <= yMax + 1; y++) {
+            for (let z = zMin - 1; z <= zMax + 1; z++) {
+                const value = cubes.find(
+                    (c) => c.x === x && c.y === y && c.z === z
+                )
+                    ? "lava"
+                    : "water";
 
-    for (let x = xMin; x <= xMax; x++) {
-        for (let y = yMin; y <= yMax; y++) {
-            for (let z = zMin; z <= zMax; z++) {
-                const xBefore = cubes.filter(
-                    (c) => c.x < x && c.y === y && c.z === z
-                );
-
-                const xAfter = cubes.filter(
-                    (c) => c.x > x && c.y === y && c.z === z
-                );
-
-                const yBefore = cubes.filter(
-                    (c) => c.x === x && c.y < y && c.z === z
-                );
-
-                const yAfter = cubes.filter(
-                    (c) => c.x === x && c.y > y && c.z === z
-                );
-
-                const zBefore = cubes.filter(
-                    (c) => c.x === x && c.y === y && c.z < z
-                );
-
-                const zAfter = cubes.filter(
-                    (c) => c.x === x && c.y === y && c.z > z
-                );
-
-                // check if the space is surrounded
-                if (
-                    xBefore.length &&
-                    xAfter.length &&
-                    yBefore.length &&
-                    yAfter.length &&
-                    zBefore.length &&
-                    zAfter.length
-                ) {
-                    if (
-                        !cubes.find((c) => c.x === x && c.y === y && c.z === z)
-                    ) {
-                        toIgnoreSpace.push({ x, y, z });
-                    }
-                }
-
-                // fill empty spaces with blank
-                // addCube({ x, y, z }, "blank");
+                coordinates.push({ x, y, z, value });
             }
         }
     }
 
-    return calculateSurfaceArea(cubes) - calculateSurfaceArea(toIgnoreSpace);
+    let nodes = [
+        coordinates.find(
+            (point) => point.x === xMin && point.y === yMin && point.z === zMin
+        ),
+    ];
 
-    function calculateSurfaceArea(cubes) {
-        let surfaceArea = 6 * cubes.length;
+    const visited = [];
 
-        cubes.forEach((c1) => {
-            // add each cube to the 3d space
-            addCube(c1);
-            cubes
-                .filter((c2) => getDistance(c1, c2) === 1)
-                .forEach(() => {
-                    surfaceArea -= 1;
-                });
-        });
+    let totalSurface = 0;
 
-        return surfaceArea;
+    while (nodes.length) {
+        const node = nodes.pop();
+        visited.push(node);
+
+        const neighbours = coordinates.filter(
+            (cube) => getDistance(cube, node) === 1
+        );
+
+        const lava = neighbours.filter((cube) => cube.value === "lava");
+
+        totalSurface += lava.length;
+
+        const waterNeighbours = neighbours
+            .filter((cube) => cube.value === "water")
+            .filter((cube) => !visited.includes(cube));
+
+        nodes = [...new Set([...nodes, ...waterNeighbours])];
     }
+
+    return totalSurface;
 
     function getDistance(c1, c2) {
         return Math.sqrt(
@@ -106,21 +77,5 @@ function main(puzzleInput) {
                 Math.pow(Math.abs(c1.y - c2.y), 2) +
                 Math.pow(Math.abs(c1.z - c2.z), 2)
         );
-    }
-
-    function addCube(cube, value = "cube") {
-        const { x, y, z } = cube;
-
-        if (!space[x]) {
-            space[x] = [];
-        }
-
-        if (!space[x][y]) {
-            space[x][y] = [];
-        }
-
-        if (!space[x][y][z]) {
-            space[x][y][z] = value;
-        }
     }
 }
