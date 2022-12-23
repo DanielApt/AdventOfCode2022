b8891e4794e81474215e1e2403cf1978b067f8df
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:48 +0000

    Day 20 - start part 2

--- day-20/part-2.js
+++ day-20/part-2.js
@@ -11,5 +11,39 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    return 0;
+    const initialArrangement = puzzleInput.split("\n").map((str, index) => ({
+        id: index,
+        value: Number(str),
+    }));
+
+    let arrangement = [...initialArrangement];
+
+    initialArrangement.forEach((item) => {
+        const index = arrangement.indexOf(item);
+        const movement = item.value;
+
+        // remove the item being moved
+        const oldItem = arrangement.splice(index, 1)[0];
+
+        // add it to the place we are interested in
+        let newIndex = index + movement;
+        while (newIndex <= 0) {
+            newIndex += arrangement.length;
+        }
+
+        while (newIndex > arrangement.length) {
+            newIndex -= arrangement.length;
+        }
+
+        arrangement.splice(newIndex, 0, oldItem);
+
+        // console.log(arrangement.map((n) => n.value).join(", "));
+    });
+
+    const zeroIndex = arrangement.findIndex((n) => n.value === 0);
+    const a = arrangement[(zeroIndex + 1000) % arrangement.length].value;
+    const b = arrangement[(zeroIndex + 2000) % arrangement.length].value;
+    const c = arrangement[(zeroIndex + 3000) % arrangement.length].value;
+
+    return a + b + c;
 }
