de58c9f88725e5813758ea9477865f3741e982ae
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:46 +0000

    Solve day 2 - puzzle 1

--- day-2/index.js
+++ day-2/index.js
@@ -3,15 +3,53 @@ const fs = require('fs');
 const sampleInput = fs.readFileSync('./sample-input.txt', 'utf8');
 const puzzleInput = fs.readFileSync('./input.txt', 'utf8');
 
-const {splitLinesToNumbers} = require('../util');
+const CHOICE_POINTS = {
+    A: 1,
+    B: 2,
+    C: 3,
+    X: 1,
+    Y: 2,
+    Z: 3
+}
+
+const OUTCOMES = {
+    A: {
+        X: 3,
+        Y: 6,
+        Z: 0
+    },
+    B: {
+        X: 0,
+        Y: 3,
+        Z: 6
+    },
+    C: {
+        X: 6,
+        Y: 0,
+        Z: 3
+    }
+}
 
 console.log(main(sampleInput));
 console.log(main(puzzleInput));
 
 /**
  * @param {String} puzzleInput
- * @return {Number} calories of largest amount
+ * @return {Number} total score
  */
 function main(puzzleInput) {
-    return 0;
+    const games = puzzleInput.split('\n').filter(str => str !== '').map(str => [str.charAt(0), str.charAt(2)]);
+    let score = 0;
+
+    games.forEach(game => {
+        const [opponentMove, yourMove] = game;
+
+        console.log(`Adding ${CHOICE_POINTS[yourMove]} for choice`)
+        score += CHOICE_POINTS[yourMove];
+
+        console.log(`Adding ${OUTCOMES[opponentMove][yourMove]} for game`)
+        score += OUTCOMES[opponentMove][yourMove];
+    });
+
+    return score;
 }
