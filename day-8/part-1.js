89f292d2a72f72eca166d75227c76eac48bf0c58
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:47 +0000

    Day 8 - sample working, not yet part 1

--- day-8/part-1.js
+++ day-8/part-1.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
+// 9546 - answer too high
+
 console.log(main(sampleInput));
 console.log(main(puzzleInput));
 
@@ -11,5 +13,56 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    return 0;
+    const grid = puzzleInput.split('\n').map(row => row.split('').map(str => Number(str)));
+    let visibleTrees = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (i === 0 || i === grid.length - 1 || j === 0 || j === grid[0].length - 1) {
+                visibleTrees++;
+                continue;
+            }
+
+            const tree = grid[i][j];
+
+            // look left
+            let leftBigger = false;
+            for (let left = i; left > 1; left--) {
+                if(grid[left][j] > tree) {
+                    leftBigger = true;
+                    break;
+                }
+            }
+
+            let rightBigger = false;
+            for (let right = i; right < grid[0].length - 1; right++) {
+                if(grid[right][j] > tree) {
+                    rightBigger = true;
+                    break;
+                }
+            }
+
+            let topBigger = false;
+            for (let top = j; top > 1; top--) {
+                if(grid[i][top] > tree) {
+                    topBigger = true;
+                    break;
+                }
+            }
+
+            let bottomBigger = false;
+            for (let bottom = j; bottom < grid.length - 1; bottom++) {
+                if(grid[i][bottom] > tree) {
+                    bottomBigger = true;
+                    break;
+                }
+            }
+
+            if (leftBigger || rightBigger || topBigger || bottomBigger) {
+                visibleTrees++;
+            }
+        }
+    }
+
+    return visibleTrees;
 }
