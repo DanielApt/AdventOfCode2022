c9b9cadc94ddadf32c7c55f586fe7bbbe94a1852
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:48 +0000

    Start 18 part 2

--- day-18/part-2.js
+++ day-18/part-2.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
+const CUBE_SURFACE = 6;
+
+console.log(main(`1,1,1\n2,1,1`));
 console.log(main(sampleInput));
 console.log(main(puzzleInput));
 
@@ -11,5 +14,32 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    return 0;
+    let totalSurface = 0;
+
+    const cubes = puzzleInput
+        .split("\n")
+        .map((s) => s.split(",").map((s) => Number(s)))
+        .map(([x, y, z]) => ({ x, y, z }));
+
+    totalSurface += CUBE_SURFACE * cubes.length;
+
+    // TODO: Remove cubes which are contained
+
+    cubes.forEach((c1) => {
+        cubes
+            .filter((c2) => getDistance(c1, c2) === 1)
+            .forEach(() => {
+                totalSurface -= 1;
+            });
+    });
+
+    return totalSurface;
+
+    function getDistance(c1, c2) {
+        return Math.sqrt(
+            Math.pow(Math.abs(c1.x - c2.x), 2) +
+                Math.pow(Math.abs(c1.y - c2.y), 2) +
+                Math.pow(Math.abs(c1.z - c2.z), 2)
+        );
+    }
 }
