b2246e790831e8ce413131b609d4c97d6bf65b9c
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:47 +0000

    Solve day 8 - part 2

--- day-8/part-2.js
+++ day-8/part-2.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
+// 9546 - answer too high
+// 9447 - answer too high
+
 console.log(main(sampleInput));
+
 console.log(main(puzzleInput));
 
 /**
@@ -11,5 +15,79 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    return 0;
+    const grid = puzzleInput
+        .split("\n")
+        .map((row) => row.split("").map((str) => Number(str)));
+    let visibleTrees = 0;
+
+    const visibleScores = [];
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            const left = grid[i].slice(0, j).reverse();
+            const right = grid[i].slice(j + 1, grid[i].length);
+            const top = grid.slice(0, i).map((row) => row[j]).reverse();
+            const bottom = grid.slice(i + 1, grid.length).map((row) => row[j]);
+
+            if (
+                left.length === 0 ||
+                top.length === 0 ||
+                right.length === 0 ||
+                bottom.length === 0
+            ) {
+                continue;
+            }
+
+            const tree = grid[i][j];
+
+            const leftVisible = getVisibleTrees(left, tree);
+            const topVisible = getVisibleTrees(top, tree);
+            const rightVisible = getVisibleTrees(right, tree);
+            const bottomVisible = getVisibleTrees(bottom, tree);
+
+            const visibleScore = [
+                leftVisible,
+                topVisible,
+                rightVisible,
+                bottomVisible,
+            ]
+                .filter((score) => score !== 0)
+                .reduce((a, b) => a * b);
+            visibleScores.push(visibleScore);
+
+            // visibleScores.push(leftVisible * topVisible * rightVisible * bottomVisible);
+
+            //
+            // if (
+            //     tree > getBiggestItemFromArray(left) ||
+            //     tree > getBiggestItemFromArray(top) ||
+            //     tree > getBiggestItemFromArray(right) ||
+            //     tree > getBiggestItemFromArray(bottom)
+            // ) {
+            //     visibleTrees++;
+            // }
+        }
+    }
+
+    return getBiggestItemFromArray(visibleScores);
+
+    return visibleTrees;
+}
+
+function getVisibleTrees(arr, current) {
+    let visibleTrees = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= current) {
+            visibleTrees++;
+            break;
+        } else {
+            visibleTrees++;
+        }
+    }
+
+    return visibleTrees;
+}
+
+function getBiggestItemFromArray(arr) {
+    return arr.sort((a, b) => b - a)[0];
 }
