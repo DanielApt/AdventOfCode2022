6ce4580c4a0f87be5528ff621c5ecd21e26719d0
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:47 +0000

    Solve day 8 - part 1

--- day-8/part-1.js
+++ day-8/part-1.js
@@ -14,56 +14,44 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    const grid = puzzleInput.split('\n').map(row => row.split('').map(str => Number(str)));
+    const grid = puzzleInput
+        .split("\n")
+        .map((row) => row.split("").map((str) => Number(str)));
     let visibleTrees = 0;
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (i === 0 || i === grid.length - 1 || j === 0 || j === grid[0].length - 1) {
+            const left = grid[i].slice(0, j);
+            const right = grid[i].slice(j + 1, grid[i].length);
+            const top = grid.slice(0, i).map((row) => row[j]);
+            const bottom = grid.slice(i + 1, grid.length).map((row) => row[j]);
+
+            const tree = grid[i][j];
+
+            if (
+                left.length === 0 ||
+                top.length === 0 ||
+                right.length === 0 ||
+                bottom.length === 0
+            ) {
                 visibleTrees++;
                 continue;
             }
 
-            const tree = grid[i][j];
-
-            // look left
-            let leftBigger = false;
-            for (let left = i; left > 1; left--) {
-                if(grid[left][j] > tree) {
-                    leftBigger = true;
-                    break;
-                }
-            }
-
-            let rightBigger = false;
-            for (let right = i; right < grid[0].length - 1; right++) {
-                if(grid[right][j] > tree) {
-                    rightBigger = true;
-                    break;
-                }
-            }
-
-            let topBigger = false;
-            for (let top = j; top > 1; top--) {
-                if(grid[i][top] > tree) {
-                    topBigger = true;
-                    break;
-                }
-            }
-
-            let bottomBigger = false;
-            for (let bottom = j; bottom < grid.length - 1; bottom++) {
-                if(grid[i][bottom] > tree) {
-                    bottomBigger = true;
-                    break;
-                }
-            }
-
-            if (leftBigger || rightBigger || topBigger || bottomBigger) {
+            if (
+                tree > getBiggestItemFromArray(left) ||
+                tree > getBiggestItemFromArray(top) ||
+                tree > getBiggestItemFromArray(right) ||
+                tree > getBiggestItemFromArray(bottom)
+            ) {
                 visibleTrees++;
             }
         }
     }
 
     return visibleTrees;
+}
+
+function getBiggestItemFromArray(arr) {
+    return arr.sort((a, b) => b - a)[0];
 }
