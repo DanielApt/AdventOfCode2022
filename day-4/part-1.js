ce51edcac2b14f74ba49f5c133540eeddde28b8b
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:46 +0000

    Solve day 4 - part 1

---
+++ day-4/part-1.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
+const puzzleInput = fs.readFileSync("./input.txt", "utf8");
+
+console.log(main(sampleInput));
+console.log(main(puzzleInput));
+
+/**
+ * @param {String} puzzleInput
+ * @return {Number}
+ */
+function main(puzzleInput) {
+    const ranges = puzzleInput
+        .split("\n")
+        .map((str) => str.match(/(\d+)-(\d+),(\d+)-(\d+)/))
+        .map((result) => [
+            [Number(result[1]), Number(result[2])],
+            [Number(result[3]), Number(result[4])],
+        ]);
+
+    let totalRanges = 0;
+
+    ranges.forEach(range => {
+        const a = range[0];
+        const b = range[1];
+
+        if (a[0] <= b[0] && a[1] >= b[1] ) {
+            totalRanges += 1;
+        } else if (b[0] <= a[0] && b[1] >= a[1]) {
+            totalRanges +=1;
+        }
+    });
+
+    return totalRanges;
+}
