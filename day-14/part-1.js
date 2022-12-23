1b6921bea892d2206b645b5edc7c6d15256a3481
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:48 +0000

    Save inputs

--- day-14/part-1.js
+++ day-14/part-1.js
@@ -4,12 +4,14 @@ const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
 console.log(main(sampleInput));
-console.log(main(puzzleInput));
+// console.log(main(puzzleInput));
 
 /**
  * @param {String} puzzleInput
  * @return {Number}
  */
 function main(puzzleInput) {
+    const rocks = puzzleInput.split("\n").map((str) => str);
+    console.log(rocks);
     return 0;
 }
