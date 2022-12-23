9f7b7a7776f6511500df3b012b6f45221369d81b
Daniel Apt <info@iamapt.com>
2022-12-23 23:38:47 +0000

    Solve day 6 - part 2

--- day-6/part-2.js
+++ day-6/part-2.js
@@ -3,7 +3,12 @@ const fs = require("fs");
 const sampleInput = fs.readFileSync("./sample-input.txt", "utf8");
 const puzzleInput = fs.readFileSync("./input.txt", "utf8");
 
-console.log(main(sampleInput));
+const OFFSET = 14;
+
+console.log(main('mjqjpqmgbljsphdztnvjfqwrcgsmlb'));
+console.log(main('bvwbjplbgvbhsrlpgdmjqwftvncz'));
+console.log(main('nppdvjthqldpwncqszvftbrmjlhg'));
+
 console.log(main(puzzleInput));
 
 /**
@@ -11,5 +16,17 @@ console.log(main(puzzleInput));
  * @return {Number}
  */
 function main(puzzleInput) {
-    return 0;
+    const characters = puzzleInput.split('');
+    let markerIndex = 0;
+
+    for (let i = 0; i < characters.length; i++) {
+        const part = characters.slice(i, i + OFFSET);
+
+        if (new Set([...part]).size === OFFSET) {
+            markerIndex = i + OFFSET;
+            break;
+        }
+    }
+
+    return markerIndex;
 }
